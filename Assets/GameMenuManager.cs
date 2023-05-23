824366eaa1aee36f2beae55efcaaba95517c5aa7
phaepich <sasha-popov04@list.ru>
2023-05-23 13:53:56 +0300

    pause menu

--- Assets/GameMenuManager.cs
+++ Assets/GameMenuManager.cs
@@ -7,7 +7,7 @@ public class GameMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject _menu;
     [SerializeField] private InputActionProperty _showButton;
-    [SerializeField] private Transform _head;
+    [SerializeField] private Transform head;
     [SerializeField] private float _spawnDistance = 2;
     
     void Start()
@@ -22,10 +22,10 @@ public class GameMenuManager : MonoBehaviour
         if (_showButton.action.WasPressedThisFrame())
         {
             _menu.SetActive(!_menu.activeSelf);
-            _menu.transform.position = _head.position +
-                                       new Vector3(_head.forward.x, 0, _head.forward.z).normalized * _spawnDistance;
+            _menu.transform.position = head.position +
+                                       new Vector3(head.forward.x, 0, head.forward.z).normalized * _spawnDistance;
         }
-        _menu.transform. LookAt(new Vector3(_head.position.x, _menu.transform.position.y, _head.position.z));
+        _menu.transform.LookAt(new Vector3(head.position.x, _menu.transform.position.y, head.position.z));
         _menu.transform.forward *= -1;
     }
 }
